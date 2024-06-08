40fbe58ccfa69ae74f0254dc8e817a0ca1ac5783
pedroGV90 <pedro.guerrero@legis.com.co>
2024-06-08 00:01:50 -0500

    ajustes navegacion inmofinanzaAGV

--- src/componentes/cabecera/Cabecera.jsx
+++ src/componentes/cabecera/Cabecera.jsx
@@ -6,7 +6,7 @@ function Cabecera() {
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
                 <img src="/imagenes/InmoFinanzas.png" className='navbar-logo' alt="InmoFinanzas Logo" />
-                <a class="navbar-brand" href="/">Navbar</a>
+                <a class="navbar-brand" href="/">InmoFinanzasAGV</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
