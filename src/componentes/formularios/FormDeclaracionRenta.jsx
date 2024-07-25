fcb203fa8ce5eb54922d897b95cd4d864595e4dd
pedroGV90 <pedro.guerrero@legis.com.co>
2024-07-24 21:30:19 -0500

    ajuste para ver las fechas de declaracion

--- src/componentes/formularios/FormDeclaracionRenta.jsx
+++ src/componentes/formularios/FormDeclaracionRenta.jsx
@@ -2,6 +2,61 @@ import React, { useState } from 'react';
 import { apiUrl } from '../../config';
 
 function FormDeclaracionRenta() {
+    const [dateLabel, setDateLabel] = useState('');
+
+    const dates = [
+        [[1, 2], "12 de agosto del 2024"],
+        [[3, 4], "13 de agosto del 2024"],
+        [[5, 6], "14 de agosto del 2024"],
+        [[7, 8], "15 de agosto del 2024"],
+        [[9, 10], "16 de agosto del 2024"],
+        [[11, 12], "20 de agosto del 2024"],
+        [[13, 14], "21 de agosto del 2024"],
+        [[15, 16], "22 de agosto del 2024"],
+        [[17, 18], "23 de agosto del 2024"],
+        [[19, 20], "26 de agosto del 2024"],
+        [[21, 22], "27 de agosto del 2024"],
+        [[23, 24], "28 de agosto del 2024"],
+        [[25, 26], "29 de agosto del 2024"],
+        [[27, 28], "2 de septiembre del 2024"],
+        [[29, 30], "3 de septiembre del 2024"],
+        [[31, 32], "4 de septiembre del 2024"],
+        [[33, 34], "5 de septiembre del 2024"],
+        [[35, 36], "6 de septiembre del 2024"],
+        [[37, 38], "9 de septiembre del 2024"],
+        [[39, 40], "10 de septiembre del 2024"],
+        [[41, 42], "11 de septiembre del 2024"],
+        [[43, 44], "12 de septiembre del 2024"],
+        [[45, 46], "13 de septiembre del 2024"],
+        [[47, 48], "16 de septiembre del 2024"],
+        [[49, 50], "17 de septiembre del 2024"],
+        [[51, 52], "18 de septiembre del 2024"],
+        [[53, 54], "19 de septiembre del 2024"],
+        [[55, 56], "20 de septiembre del 2024"],
+        [[57, 58], "23 de septiembre del 2024"],
+        [[59, 60], "24 de septiembre del 2024"],
+        [[61, 62], "25 de septiembre del 2024"],
+        [[63, 64], "26 de septiembre del 2024"],
+        [[65, 66], "27 de septiembre del 2024"],
+        [[67, 68], "1 de octubre del 2024"],
+        [[69, 70], "2 de octubre del 2024"],
+        [[71, 72], "3 de octubre del 2024"],
+        [[73, 74], "4 de octubre del 2024"],
+        [[75, 76], "7 de octubre del 2024"],
+        [[77, 78], "8 de octubre del 2024"],
+        [[79, 80], "9 de octubre del 2024"],
+        [[81, 82], "10 de octubre del 2024"],
+        [[83, 84], "11 de octubre del 2024"],
+        [[85, 86], "15 de octubre del 2024"],
+        [[87, 88], "16 de octubre del 2024"],
+        [[89, 90], "17 de octubre del 2024"],
+        [[91, 92], "18 de octubre del 2024"],
+        [[93, 94], "21 de octubre del 2024"],
+        [[95, 96], "22 de octubre del 2024"],
+        [[97, 98], "23 de octubre del 2024"],
+        [[99, 0], "24 de octubre del 2024"],
+    ];
+
     const [formData, setFormData] = useState({
         nombre: '',
         telefono: '',
@@ -15,12 +70,29 @@ function FormDeclaracionRenta() {
         patrimonio: ''
     });
 
+    const detectFechaDeclaracion = (value) => {
+        if (value.length >= 2) {
+            const firstTwoChars = parseInt(value.slice(0, 2), 10);
+            const matchingDate = dates.find(([range]) => range.includes(firstTwoChars));
+            if (matchingDate) {
+                setDateLabel(matchingDate[1]);
+            } else {
+                setDateLabel('No date found for the entered value');
+            }
+        } else {
+            setDateLabel('');
+        }
+    };
+
     const handleChange = (e) => {
         const { id, value } = e.target;
         setFormData((prevData) => ({
             ...prevData,
             [id]: value
         }));
+        if (id === 'cedula') {
+            detectFechaDeclaracion(value);
+        }
     };
 
     const handleSubmit = async (e) => {
@@ -82,6 +154,8 @@ function FormDeclaracionRenta() {
                                 <div className="mb-3">
                                     <label htmlFor="cedula" className="form-label">Últimos 2 dígitos de la cédula</label>
                                     <input id="cedula" type="number" className="form-control" placeholder="00" value={formData.cedula} onChange={handleChange} />
+                                    {dateLabel && <label>Ultimo dia para presentar la declaracion: <strong style={{ color: 'green' }}>{dateLabel}</strong></label> }
+
                                 </div>
                             </div>
                             <div className="col-md-6">
